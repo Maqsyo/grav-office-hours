216015859900f1361ccd8dcedae4679ca03205a1
Maqsyo <max-panzer@wiaazo.de>
2022-01-28 15:16:08 +0100

    Add first draft of blueprint for admin-ui

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '09b32e3339024c75554e23bb034799a0a7108bc2',
+        'name' => 'max-panzer/office-hours',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'max-panzer/office-hours' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '09b32e3339024c75554e23bb034799a0a7108bc2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
